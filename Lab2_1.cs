58b5c5695ea6e5d2794d658c58211e5a8be7e47f
dementevt <46136992+dementevt@users.noreply.github.com>
2022-12-07 13:13:52 +0400

    Циклы

--- Lab2_1.cs
+++ Lab2_1.cs
@@ -8,15 +8,15 @@ namespace ConsoleApplication1
 		static void Main(string[] args)
 		{
 			int number = int.Parse(ReadLine());
-			int count = 0;
+			int c = 0;
 			while (number > 1)
 			{
 				if (number % 2 == 0)
 					number /= 2;
 				else number = number * 3 + 1;
-				count++;
+				c++;
 			}
-			WriteLine($"Количество необходимых действий над числом - {count}");
+			WriteLine($"Количество необходимых действий над числом - {c");
 		}
 	}
 }
