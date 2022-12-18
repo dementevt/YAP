16a19a0935a32bf8cfd3e828ef4dfabe744c9e54
dementevt <46136992+dementevt@users.noreply.github.com>
2022-12-18 23:11:34 +0400

    Add files via upload

---
+++ Массивы/Lab1_2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Write("Введите текст: ");
+			string text = ReadLine();
+			string[] words = text.Split(' ');
+			foreach (string word in words)
+			{
+				foreach (char letter in word)
+					Console.WriteLine(letter);
+				Console.WriteLine();
+			}
+		}
+	}
+}
