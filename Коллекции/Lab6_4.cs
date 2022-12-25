7bb2f748a61cf34fb56db7d54414cc9613a34113
dementevt <46136992+dementevt@users.noreply.github.com>
2022-12-26 00:00:53 +0400

    Update Lab6_4.cs

--- Коллекции/Lab6_4.cs
+++ Коллекции/Lab6_4.cs
@@ -9,8 +9,15 @@ namespace ConsoleApplication1
 	{
 		static void Main(string[] args)
 		{
-			List<int> list = new List<int> {1, 2, 5, 7, 8, 10};
+			List<int> list = new List<int> ();
 			int c = 0;
+			string a = Console.ReadLine();
+			string[] b = a.Split(",");
+			for (int i = 0; i < b.Length; i++)
+			{
+				list.Add(int.Parse(b[i]));
+			}
+
 			for (int i = list[0]; i < list[list.Count-1]; i++)
 			{
 				if (i != list[c])
