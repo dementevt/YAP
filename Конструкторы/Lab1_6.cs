f87a545572b164b68d1f9ec50835f25656060df7
dementevt <46136992+dementevt@users.noreply.github.com>
2022-12-26 01:48:56 +0400

    Add files via upload

---
+++ Конструкторы/Lab1_6.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var myWatch = new Watch("Casio", "electronic", 15.99M);
+			myWatch.Print();
+		}
+	}
+	
+	class Watch
+	{
+		public string brand, type;
+		public decimal  num;
+		public Watch(string brand, string type, decimal num)
+		{
+			this.brand = brand;
+			this.type = type;
+			this.num = num;
+		}
+		
+		public void Print()
+		{
+			Console.WriteLine($"{brand} {type} {num}");
+		}
+	}
+}
